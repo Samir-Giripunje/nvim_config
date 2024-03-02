1acd7791a9e2e240a0ef3c77f20e56a9f7a84b95
Samir-Giripunje <samirgiripunje5@gmail.com>
2024-03-02 23:34:47 +0530

    New plugins and files are added

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -1,25 +1,39 @@
 return {
-  "MunifTanjim/prettier.nvim",
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
   config = function()
-    local status, prettier = pcall(require, "prettier")
-    if not status then return end
+    local conform = require("conform")
 
-    prettier.setup {
-      bin = 'prettierd',
-      filetypes = {
-        "css",
-        "html",
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "json",
-        "scss",
-        "less",
-        "markdown",
-        "yaml",
-        "lua",
-      }
-    }
-  end
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "=", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
 }
